092d9d7154f7e56e41ec9e0ad6a7022ea7ed7c04
Ali Zaidi <alihasnain09@hotmail.com>
2021-12-02 19:14:52 -0500

    Create LICENSE
    Update the README and source files to display the license.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # OracleDB-Viewer
 Python GUI for Oracle Databases
+
+## License
+This project is licensed under terms of the GNU General Public License v3.0. See the [LICENSE](LICENSE.md) file for more information.
--- src/ConnectionDialog.py
+++ src/ConnectionDialog.py
@@ -1,3 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+OracleDB Viewer, A GUI for Oracle Database
+Copyright (C) 2021  Ali Zaidi
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 import tkinter as tk
 from tkinter.simpledialog import Dialog
 from tkinter.messagebox import showinfo, showerror
